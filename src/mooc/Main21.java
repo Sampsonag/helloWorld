4bca528dadb3a6adec1e09da9eb196ec94ce60a6
Aaron Sampson <asampso2@ITS.JNJ.com>
2018-10-01 08:17:04 -0400

    Small updates from the weekend before 10-1

--- src/mooc/Main21.java
+++ src/mooc/Main21.java
@@ -3,7 +3,12 @@ package mooc;
 public class Main21 {
 	
 	public static void main(String[] args) {
-	
+		Person21 Tom = new Person21("Tom", 5);
+		
+		Tom.printPerson();
+		
+		
+		
 		
 	}
 	
--- src/mooc/Person21.java
+++ src/mooc/Person21.java
@@ -2,7 +2,20 @@ package mooc;
 
 public class Person21 {
 	
-	private String name;
+	private String name; // private only showing inside the object
     private int age;
+	/**
+	 * @param name
+	 * @param age
+	 */
+	public Person21(String name) {
+		super();
+		this.name = name;
+		this.age = 0;
+	}
+    
+	 public void printPerson() {
+	        System.out.println(this.name + ", age " + this.age + " years");
+	    }
 
 }
